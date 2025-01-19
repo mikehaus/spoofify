06896cb4e7964fd05d3c72bde5f7bf7e4d246fcd
mikehaus <mike.haus.dev@gmail.com>
2025-01-19 11:08:34 -0700

    Initial commit spotify cli app

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// First we need to check if user is authorized
+// If they are direct them to the main window
+// If not do signup flow
+
+type model int
+
+type tickMsg time.Time
+
+func main() {
+	p := tea.NewProgram(model(5), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return tick()
+}
+
+func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := message.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl+c":
+			return m, tea.Quit
+		}
+
+	case tickMsg:
+		m--
+		if m <= 0 {
+			return m, tea.Quit
+		}
+		return m, tick()
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("\n\n     Hi. This program will end in %d seconds...", m)
+}
+
+func tick() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
+}
